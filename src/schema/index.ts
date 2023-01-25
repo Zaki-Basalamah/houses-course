d6b0fd01c601fe11b3f141d2615d0b6aa2e1db89
Zaki Basalamah <zacy.n89@gmail.com>
2023-01-25 11:31:55 +0700

    Initial commit

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,8 +3,15 @@ import { ImageResolver } from "./image";
 import { HouseResolver } from "./house";
 import { authChecker } from "./auth";
 
+@Resolver()
+class DummyResolver {
+  hello() {
+    return "Nice to meet you!";
+  }
+}
+
 export const schema = buildSchemaSync({
-  resolvers: [ImageResolver, HouseResolver],
+  resolvers: [DummyResolver],
   emitSchemaFile: process.env.NODE_ENV === "development",
   authChecker,
 });
